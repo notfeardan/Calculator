7eafe688ec1eee76993010a221d816b51455da7c
Daniil Baturyn <notfeardan@gmail.com>
2025-01-24 00:03:15 -0600

    Added event listeners for operators and numbers

---
+++ script.js
@@ -0,0 +1,25 @@
+const caculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+
+function sendNumberValue(number) {
+  const displayValue = caculatorDisplay.textContent;
+  caculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => {
+      sendNumberValue(inputBtn.value);
+    });
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => {
+      sendNumberValue(inputBtn.value);
+    });
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => {
+      sendNumberValue(inputBtn.value);
+    });
+  }
+});
